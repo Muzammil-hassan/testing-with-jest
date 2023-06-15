3c7486b6b8ffe1f062652e3685c5219a5482784d
muzammil-hasan <muzammil.hassan1@techchefz.com>
2023-06-15 15:26:16 +0530

    feat: test coverage added

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,15 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleDirectories: ['node_modules', '<rootDir>/'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+    '!**/coverage/**',
+    '!**/jest.config.js',
+    '!**/next.config.js',
+  ],
   clearMocks: true,
 };
 
